e0ba70b7c8ddf214627b14c313a95501cc9f5596
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-11 05:38:05 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   
   while array[counter] do
     if array[counter] == value_to_find
-      puts array.index(value_to_find)
+      return counter
   end
   count += 1
 end
